b698b8c9061138a0b4ff9e0d1d1a9b5782cced44
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-03-06 21:57:03 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navber from './components/Navber'
+
+const App = () => {
+  return (
+    <>
+    <Navber/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navber.jsx
@@ -0,0 +1,22 @@
+import logo from '../../src/assets/Images/logo/Navber_logo.png'
+
+const Navber = () => {
+  return (
+    <>
+    <nav>
+        <div className="conatiner">
+            <div>
+            <div className="">
+                <img src={logo} alt="" />
+            </div>
+            <div className=""></div>
+            <div className=""></div>
+            <div className=""></div>
+        </div>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navber
