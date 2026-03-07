d986f806aef80bc966927d87eaff8ee9f1fda763
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-03-07 07:52:58 +0600

    Items add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navber from './components/Navber'
 import Banner from './components/Banner'
+import Items from './components/Items'
 
 const App = () => {
   return (
     <>
     <Navber/>
     <Banner/>
+    <Items/>
     </>
   )
 }
---
+++ src/components/Items.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import peyaj from '../../src/assets/Images/Items/peyaj.png'
+import cal from '../../src/assets/Images/Items/cal.png'
+import alu from '../../src/assets/Images/Items/alu.png'
+import moric from '../../src/assets/Images/Items/moric.png'
+import tel from '../../src/assets/Images/Items/tel.png'
+const Items = () => {
+  return (
+    <>
+    <section>
+      <div className="container">
+        
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default Items
